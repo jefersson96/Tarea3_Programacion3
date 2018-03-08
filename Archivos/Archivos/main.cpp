e744a43680e8b1c2220d18a5d636f9a673a7cd9a
jefersson96 <32781293+jefersson96@users.noreply.github.com>
2018-03-08 11:59:14 -0600

    Tarea3

--- Archivos/Archivos/main.cpp
+++ Archivos/Archivos/main.cpp
@@ -1,15 +1,78 @@
 #include <conio.h>
+#include<iostream>
 #include"Notas.h"
+using namespace std;
 int main() {
-	Notas::registrarAlumnos();
-	Notas::consultarAlumnos();
+
+	//Notas::registrarAlumnos();
+	//Notas::consultarAlumnos();
 
 	//Notas::registrarMateria();
-	Notas::consultarMateria();
+	//Notas::consultarMateria();
 	//Notas::registrarNotas();
 	//Notas::registrarNotas();
 	//Notas::consultarNotas();
 
 	//Notas::promedioMateria();
+
+	int opcion;
+	do {
+		cout << "\n\n *** M E N U ** \n\n";
+		cout << "\n1-Ingresar Materia\n2-Ingresar Alumno\n3-Ingresar Nota\n4-Mostrar Materias\n5-Mostrar Alumnos\n6-Mostrar Notas\n7-Consultar Promedio Alumno\n8-Consultar promedio clase\n9-Salir" << endl;
+		cout << "Ingrese una opcion: " << endl;
+		cin >> opcion;
+
+
+		switch (opcion)
+		{
+
+		case 1:
+			system("cls");
+			Notas::registrarMateria();
+			break;
+		case 2:
+			system("cls");
+			Notas::registrarAlumnos();
+
+			break;
+		case 3:
+			system("cls");
+			Notas::registrarNotas();
+			break;
+		case 4:
+			Notas::consultarMateria();
+			break;
+
+		case 5:
+			system("cls");
+			Notas::consultarAlumnos();
+
+			break;
+		case 6:
+			system("cls");
+			Notas::consultarNotas();
+
+			break;
+
+		case 7:
+			//	Notas::promedioAlumno();
+			break;
+		case 8:
+			system("cls");
+			Notas::promedioMateria();
+			break;
+		case 9:
+
+			exit(0);
+			break;
+		}
+
+
+		system("cls");
+	} while (opcion != 9);
+
+
+
+
 	_getch();
 }
